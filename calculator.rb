f3967eb4e036e5176ef864a7684a1b2b5babac0a
danielboyle <danielboyle111@gmail.com>
2015-04-28 17:27:10 -0400

    Added error for 'divide by zero' and functions

--- calculator.rb
+++ calculator.rb
@@ -1,34 +1,48 @@
+# Simple Calculator
+# Daniel Boyle
+
+# Functions
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def get_num
+  prompt "Please enter a number"
+  num = gets.chomp
+end
+
+# Calculator
 loop do
-  prompt "Please enter your first number"
-  num1 = gets.chomp
-
-  prompt "Please enter your second number"
-  num2 = gets.chomp
-
+  # Get user input
+  num1 = get_num
   prompt "1) add 2) subtract 3) multiply 4) divide"
   operation = gets.chomp
-
+  num2 = get_num
+  # Perform calculation
   case operation
-  when "1", "add" 
+  when "1", "add", "plus", "+"
     result = num1.to_i + num2.to_i
     prompt "#{num1} + #{num2} = #{result}"
-  when "2", "subtract"
+
+  when "2", "subtract", "minus", "-"
     result = num1.to_i - num2.to_i
     prompt "#{num1} - #{num2} = #{result}"
-  when "3", "multiply"
+
+  when "3", "multiply", "times", "*"
     result = num1.to_i * num2.to_i
     prompt "#{num1} * #{num2} = #{result}"
-  when "4", "divide"
+
+  when "4", "divide", "divided by", "/"
     result = num1.to_f / num2.to_f
     prompt "#{num1} / #{num2} = #{result}"
+    if num2 == "0"
+      prompt "ERROR. Cannot divide by 0."
+    end
+
   else
     prompt "ERROR. Please try again"
   end
-
+  # Prompt user to calculate again
   prompt "Would you like to calculate again, Y or N ?"
   calculate_again = gets.chomp
   break if calculate_again.downcase == "n"
